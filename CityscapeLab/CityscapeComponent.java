1eedb50eceb682818dad193f2c6e47992e86d66e
gcschmit <gcschmit@gmail.com>
2015-09-03 17:43:52 -0500

    refactored Cityscape lab to incorporate new requirements
    Added placeholder code to prompt the user for user-configurable
    properties of the cityscape.
    Added code to animate the cityscape.
    Added placeholder code to encourage students to define the objects in
    the cityscape as instance variables and write an appropriate
    constructor.

--- CityscapeLab/CityscapeComponent.java
+++ CityscapeLab/CityscapeComponent.java
@@ -11,6 +11,15 @@ import javax.swing.JComponent;
  */
 public class CityscapeComponent extends JComponent
 {
+    // define the objects in your Cityscape as instance variables
+    // ...
+    
+    
+    
+    // define the CityscapeComponent contructor and intiailize all instance variables
+    // ...
+    
+    
     /**
      * This method is invoked by the Java Run-Time whenever the component needs to be redrawn.
      * It does not need to be invoked explicitly.
@@ -20,10 +29,25 @@ public class CityscapeComponent extends JComponent
     {
         Graphics2D g2 = (Graphics2D) g;
         
-        // create instances of classes and invoke the draw method on each
+        // invoke the draw method on each object in your Cityscape
         // ...
         
         
+    }
+    
+    /**
+     * Animate the cityscape by updating the objects such that they appear to be animated when they are next drawn.
+     *
+     */
+    public void nextFrame()
+    {
+        // update the objects in the cityscape so they are animated
+        // ...
+        
+        
+        
+        // request that the Java Runtime repaints this component by invoking its paintComponent method
+        repaint();
     }
 
 }
--- CityscapeLab/CityscapeViewer.java
+++ CityscapeLab/CityscapeViewer.java
@@ -8,11 +8,14 @@ import javax.swing.JFrame;
  */
 public class CityscapeViewer
 {
+    // the cityscape will be animated for 60 seconds
+    static final int ANIMATION_TIME_IN_SECONDS = 60;
+   
     /**
      * main method for the program which creates and configures the frame for the program
      *
      */
-    public static void main(String[] args)
+    public static void main(String[] args) throws InterruptedException
     {
         // create and configure the frame (window) for the program
         JFrame frame = new JFrame();
@@ -21,13 +24,26 @@ public class CityscapeViewer
         frame.setTitle("Cityscape");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         
+        // use the Scanner class to prompt the user for some configurable aspect of the cityscape
+        // ...
+        
+        
+        
         // a frame contains a single component; create the Cityscape component and add it to the frame
-        CityscapeComponent component = new CityscapeComponent();
+        CityscapeComponent component = new CityscapeComponent( /* pass the user-specified value */ );
         frame.add(component);
         
         // make the frame visible which will result in the paintComponent method being invoked on the
         //  component.
         frame.setVisible(true);
+        
+        // animate the cityscape
+        for( int seconds = 0; seconds < ANIMATION_TIME_IN_SECONDS; seconds++ )
+        {
+            component.nextFrame();
+            Thread.sleep( 1000 );
+        }
+        
     }
 
 }
