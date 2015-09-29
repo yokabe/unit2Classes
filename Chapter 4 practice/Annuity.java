0854ff560cd2cebf23e4d7ce35714b2840466775
Yudai Okabe <yokabe@NN123-HA-13.HS.sd203.org>
2015-09-29 15:08:26 -0500

    More practice

--- Chapter 4 practice/Annuity.java
+++ Chapter 4 practice/Annuity.java
@@ -1,12 +1,12 @@
 public class Annuity
 {
-    public static void maths(String[] args)
+    public static void main(String[] args)
     {
-        double pmt = 10000;
-        double n = 20;
+        double pmt = 10000.0;
+        double n = 20.0;
         double i = 0.08;
         
-        double pvAnn = pmt*(((Math.pow((1+i),(n-1))/i)/((Math.pow((1+i),(n-1)))))+1);
+        double pvAnn = pmt*((((Math.pow((1+i),(n-1))-1)/i)/((Math.pow((1+i),(n-1)))))+1);
         System.out.println(pvAnn);
     }
 }
---
+++ Chapter 4 practice/Rectange.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Rectange
+{
+    public static void main(String[] args)
+    {
+        Scanner length = Scanner(System.in);
+        Scanner width = Scanner(System.in);
+        
+    }
+    
+}
